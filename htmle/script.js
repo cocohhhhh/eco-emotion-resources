c2f6491599daebc8c4df50607fc17b86f52dcd50
medimed66 <mehdi.elbouari@epfl.ch>
2023-07-29 14:45:24 +0200

    added fifth bubble

--- htmle/script.js
+++ htmle/script.js
@@ -14,3 +14,6 @@ document.getElementById('bubble1').addEventListener('click', function() {
     window.location.href = 'page4.html';
   });
   
+  document.getElementById('bubble5').addEventListener('click', function() {
+    window.location.href = 'page5.html';
+  });
