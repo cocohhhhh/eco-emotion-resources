c032312995c6e6eba811bd42fe64aa77e38c1c4e
medimed66 <mehdi.elbouari@epfl.ch>
2023-07-29 14:36:53 +0200

    update

---
+++ htmle/script.js
@@ -0,0 +1,16 @@
+document.getElementById('bubble1').addEventListener('click', function() {
+    window.location.href = 'page1.html';
+  });
+  
+  document.getElementById('bubble2').addEventListener('click', function() {
+    window.location.href = 'page2.html';
+  });
+  
+  document.getElementById('bubble3').addEventListener('click', function() {
+    window.location.href = 'page3.html';
+  });
+  
+  document.getElementById('bubble4').addEventListener('click', function() {
+    window.location.href = 'page4.html';
+  });
+  
