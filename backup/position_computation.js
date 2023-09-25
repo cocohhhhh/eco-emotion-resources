49f1a3a6f8ff941de8c4c5aedab6c616551de5a6
medimed66 <mehdi.elbouari@epfl.ch>
2023-09-25 15:00:02 +0200

    mm

---
+++ backup/position_computation.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var margin = $('.bubble.smaller').height()/1.8;
+    // Iterate over each 'bubble' class
+    $('.bubble').each(function () {
+        var bubble = $(this);
+        
+        // Find all elements with both classes 'bubble' and 'smaller' inside the current 'bubble'
+        var smallerList = bubble.find('.bubble.smaller');
+        var N = smallerList.length;
+        var R = (bubble.height() / 2 ) -margin; // Adjust this based on your desired positioning logic
+
+        // Iterate over 'smaller bubble' elements within the current 'bubble'
+        smallerList.each(function (index) {
+            var smallerBubble = $(this);
+
+            // Set the position of the smaller bubble
+            // You can modify this part to set the position as needed
+            var positionX = R * Math.cos((2 * Math.PI * index) / N + Math.PI/2); // Adjust this based on your desired positioning logic
+            var positionY = -R * Math.sin((2 * Math.PI * index) / N + Math.PI/2); // Adjust this based on your desired positioning logic
+            smallerBubble.css({
+                transform: 'translate(' + positionX + 'px, ' + positionY + 'px)'
+            });
+        });
+    });
+});
